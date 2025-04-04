99b8e7f60ef4a0fd6f094908074ba29a366c9475
Sean Stara <sean.stara1@gmail.com>
2025-04-03 22:25:43 -0500

    added code to have dices with your choice to be rolled

---
+++ Dices.py
@@ -0,0 +1,26 @@
+import random
+
+class Dices:
+    def amount_dice(self, amount, what_dice):
+        i=0
+        while i < amount:
+            dice_total = 0
+            dice_total = dice_total + self.rolling(what_dice)
+            i += 1
+        return int(dice_total)
+
+
+    def rolling(self, what_dice):
+        return int(random.randrange(1, what_dice))
+
+    def main(self):
+        modifer = input('what is your modifier? ')
+        what_dice = int(input('What dice do you want to roll? (Number) '))
+        amount = int(input('How many dice do you want to roll? '))
+        total = self.amount_dice(amount, what_dice) + int(modifer)
+        print('You got: ' + str(total))
+
+if __name__ == '__main__': 
+    Dices().main()
+
+        
